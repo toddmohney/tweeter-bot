b7b42035f72ded38b1e6872a99b7f57045cb609a
Todd Mohney <toddmohney@gmail.com>
2015-08-30 14:12:45 -0400

    Parse tweet file

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,30 @@
 module Main where
-  main = putStr "hello, I'm tweeter-bot"
+  import Control.Applicative
+
+  data TweetTree = Empty
+                   | Node TweetTree Tweet TweetTree
+
+  data Tweet = Tweet { getIndex :: Int
+                     , getTweet :: String }
+
+  -- move to env var
+  tweetFilePath :: String
+  tweetFilePath = "/Users/toddmohney/workspace/tweeter-bot/src/TweetPile"
+
+  -- use buildTweetFromString and return IO [Tweet]
+  getTweetsFromFile :: IO [String]
+  getTweetsFromFile = lines <$> readFile tweetFilePath
+
+  buildTweetFromString :: String -> Tweet
+  buildTweetFromString = undefined
+
+  buildTweetTree :: [Tweet]
+  buildTweetTree = undefined
+
+  findTweet :: TweetTree -> Int -> String
+  findTweet = undefined
+
+  main :: IO ()
+  main = putStr "hi"
+    {- tweet <- getTweet buildTweetTree 1 -}
+    {- putStr tweet -}
