b21320d17ffbc9fa11d876cb23fba53d8aa8e093
Todd Mohney <toddmohney@gmail.com>
2015-09-11 23:46:24 -0400

    Fixup merge conflicts

--- src/Main.hs
+++ src/Main.hs
@@ -7,79 +7,79 @@ module Main where
   import Data.List (intersperse)
   import Data.Maybe (mapMaybe)
   import Tweet.Tweet as Tweet
-  import qualified Tweet.TweetLogger as TL
+  import qualified Tweet.TweetLogger as TweetLogger
   import Tweet.TweetTree as TweetTree
 
   -- move to env var
   tweetFilePath :: String
-  tweetFilePath = "/Users/gust/workspace/tweeter-bot/src/TweetPile.csv"
+  tweetFilePath = "/Users/toddmohney/workspace/tweeter-bot/src/TweetPile.csv"
   -- move to env var
   tweetLogPath :: String
-  tweetLogPath = "/Users/gust/workspace/tweeter-bot/log/tweet-log.csv"
+  tweetLogPath = "/Users/toddmohney/workspace/tweeter-bot/log/tweet-log.csv"
   -- move to env var
   logPath :: String
-  logPath = "/Users/gust/workspace/tweeter-bot/log/app.log"
+  logPath = "/Users/toddmohney/workspace/tweeter-bot/log/app.log"
 
-  doTweetLoop :: TT.TweetTree -> IO ()
-  doTweetLoop TT.Empty = print "No tweets in the tweet tree!"
-  doTweetLoop tweetTree@TT.Node{} = do
+  doTweetLoop :: TweetTree -> IO ()
+  doTweetLoop Empty = print "No tweets in the tweet tree!"
+  doTweetLoop tweetTree@Node{} = do
     tweetIndex <- nextTweetIndex
     case nextTweet tweetTree tweetIndex of
       Nothing  -> print "Oh no, something went wrong!"
       (Just t) -> sendTweet t >>= logTweet
 
   -- placeholder strategy until we drop in actual tweeting
-  sendTweet :: T.Tweet -> IO (Either String T.Tweet)
-  sendTweet t = (print . T.getTweet $ t) >> (return (Right t))
+  sendTweet :: Tweet -> IO (Either String Tweet)
+  sendTweet t = (print . getTweet $ t) >> (return (Right t))
 
   -- TODO: handle failure
-  logTweet :: Either String T.Tweet -> IO ()
+  logTweet :: Either String Tweet -> IO ()
   logTweet (Left _) = undefined
-  logTweet (Right t) = TL.logTweet tweetLogPath t
+  logTweet (Right t) = TweetLogger.logTweet tweetLogPath t
 
-  nextTweet :: TT.TweetTree -> Int -> Maybe T.Tweet
-  nextTweet tree index = TT.findTweet index tree <|> TT.findFirstTweet tree
+  nextTweet :: TweetTree -> Int -> Maybe Tweet
+  nextTweet tree index = findTweet index tree <|> findFirstTweet tree
 
   nextTweetIndex :: IO Int
   nextTweetIndex = do
-    lastTweet <- TL.getLastLoggedTweet tweetLogPath 
+    lastTweet <- TweetLogger.getLastLoggedTweet tweetLogPath 
     case lastTweet of
       Nothing -> return 1
-      (Just t) -> return $ (T.getIndex t) + 1
+      (Just t) -> return $ (getIndex t) + 1
 
-  parseTweetWithLog :: [String] -> Writer [String] (Maybe T.Tweet)
+  parseTweetWithLog :: [String] -> Writer [String] (Maybe Tweet)
   parseTweetWithLog csvTweetStr = 
-    let maybeTweet = T.parseTweet csvTweetStr in
+    let maybeTweet = parseTweet csvTweetStr in
         case maybeTweet of
           Nothing  -> do
             tell ["Error parsing CSV string: " ++ (concat $ intersperse ", " csvTweetStr)]
             return Nothing
           (Just t) -> do
-            tell ["Success fully parsed tweet: " ++ (show . T.getIndex $ t)]
+            tell ["Success fully parsed tweet: " ++ (show . getIndex $ t)]
             return (Just t)
 
-  parseTweetsFromResults :: ([Maybe T.Tweet], [String]) -> [T.Tweet]
+  parseTweetsFromResults :: ([Maybe Tweet], [String]) -> [Tweet]
   parseTweetsFromResults result = mapMaybe id $ fst result
 
-  parseResultsFromResults :: ([Maybe T.Tweet], [String]) -> [String]
+  parseResultsFromResults :: ([Maybe Tweet], [String]) -> [String]
   parseResultsFromResults result = snd result
 
-  buildTweetWriter :: [[String]] -> ([Maybe T.Tweet], [String])
+  buildTweetWriter :: [[String]] -> ([Maybe Tweet], [String])
   buildTweetWriter tweetList = runWriter $ mapM parseTweetWithLog tweetList
 
-  buildTweetTree :: [T.Tweet] -> TT.TweetTree
-  buildTweetTree tweets = foldr TT.insertTweet TT.Empty tweets
+  buildTweetTree :: [Tweet] -> TweetTree
+  buildTweetTree tweets = foldr insertTweet Empty tweets
 
   main :: IO ()
   main = do
-    csvData <- parseCSV <$> readFile tweetFilePath
+    csvData       <- parseCSV <$> readFile tweetFilePath
     case csvData of
       Left e  -> Logger.log logPath $ show e
       Right t ->
-        let tweetParseResults = buildTweetWriter t
-            tweets = parseTweetsFromResults tweetParseResults
+        let tweetParseResults       = buildTweetWriter t
+            tweets                  = parseTweetsFromResults tweetParseResults
             tweetParsingLogMessages = parseResultsFromResults tweetParseResults
-            formattedLogMessages = concat $ intersperse "\n" tweetParsingLogMessages
+            formattedLogMessages    = concat $ intersperse "\n" tweetParsingLogMessages
          in
           (Logger.log logPath formattedLogMessages) >> (doTweetLoop . buildTweetTree $ tweets)
 
